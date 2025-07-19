aad32f0215df41a20d76d11c6a02e4323ad95146
Raihan Fahrel Akbar <akbarberdikari@gmail.com>
2025-07-20 00:04:10 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
 const CACHE_NAME = 'profile-card-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/img/quality_restoration_20250330050655089.jpg',
-  '/img/506986091_3639265676377448_6505629332826042357_n.jpg',
-  '/img/For Revenge - Serana (Official Lyric Video).mp3',
+  'index.html',
+  'style.css',
+  'script.js',
+  'img/quality_restoration_20250330050655089.jpg',
+  'img/506986091_3639265676377448_6505629332826042357_n.jpg',
+  'img/For Revenge - Serana (Official Lyric Video).mp3',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
   // Tambahkan path ke ikon PWA Anda di sini
-  '/img/icons/icon-192x192.png',
-  '/img/icons/icon-512x512.png',
-  '/img/icons/favicon.ico',
-  '/img/icons/apple-touch-icon.png',
-  '/img/icons/favicon-32x32.png',
-  '/img/icons/favicon-16x16.png'
+  'img/icons/icon-192x192.png',
+  'img/icons/icon-512x512.png',
+  'img/icons/favicon.ico',
+  'img/icons/apple-touch-icon.png',
+  'img/icons/favicon-32x32.png',
+  'img/icons/favicon-16x16.png'
 ];
 
 self.addEventListener('install', event => {
